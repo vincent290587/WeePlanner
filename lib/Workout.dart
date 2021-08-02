fce2c073172adf62787791d3813f1ed6603ca811
vincent290587 <vincent.golle@gmail.com>
2021-08-02 18:52:38 +0200

    Added more tests

--- lib/Workout.dart
+++ lib/Workout.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:math';
+
 import 'constants.dart';
 import 'nanopb/Workout.pb.dart';
 
@@ -13,6 +15,21 @@ import 'nanopb/Workout.pb.dart';
 double calculateNP(RawWorkout rawWorkout)
 {
   double np = 0;
+  double dur = 0;
+
+  for (var rep in rawWorkout.reps) {
+    for (var interval in rep.intervals) {
+      int? duri = interval.getField(1);
+      int? pow1 = interval.getField(2);
+      int? pow2 = interval.getField(3); // TODO
+      if (duri != null && pow1 != null && pow2 != null) {
+        np += duri * pow(pow1.toDouble(), 4);
+        dur += duri;
+      }
+    }
+  }
+
+  np = pow(np / dur, 0.25).toDouble();
 
   return np;
 }
@@ -20,6 +37,15 @@ double calculateNP(RawWorkout rawWorkout)
 int calculateDuration(RawWorkout rawWorkout)
 {
   int dur = 0;
+
+  for (var rep in rawWorkout.reps) {
+    for (var interval in rep.intervals) {
+      int? ret = interval.getField(1);
+      if (ret != null) {
+        dur += ret;
+      }
+    }
+  }
 
   return dur;
 }
@@ -36,9 +62,10 @@ class Workout {
 
   Workout(this.rawWorkout) {
 
+    duration = calculateDuration(rawWorkout);
     NP = calculateNP(rawWorkout).round();
     IF = ((100 * NP) / FTP).round();
-    TSS = (calculateDuration(rawWorkout) * NP * IF / (FTP * 3600)).round();
+    TSS = (duration * NP * IF / (FTP * 3600)).round();
   }
 
   String toString() {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,21 +1,51 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wee_planner/Workout.dart';
+import 'package:wee_planner/constants.dart';
 
 import 'package:wee_planner/nanopb/Workout.pb.dart';
 
 void main() {
-  group('Workout', () {
-    test('Power_single', () {
-      List<Repetition> reps = [
-        Repetition(intervals: [
-          Interval(duration: 3600, powerStart: 277, powerEnd: 277, cadence: 85),
-        ]),
-      ];
-      Workout workout = Workout(RawWorkout(reps: reps));
+  group('WorkoutSimple', () {
+    List<Repetition> reps = [
+      Repetition(intervals: [
+        Interval(duration: 3600, powerStart: FTP, powerEnd: FTP, cadence: 85),
+      ]),
+    ];
+    Workout workout = Workout(RawWorkout(reps: reps));
+    test('Duration', () {
+      expect(workout.duration, equals(3600));
+    });
+    test('NP', () {
+      expect(workout.NP, equals(FTP));
+    });
+    test('TSS', () {
       expect(workout.TSS, equals(100));
     });
+  });
 
+
+  group('Workout1', () {
+    List<Repetition> reps = [
+      Repetition(intervals: [
+        Interval(duration: 360, powerStart: 252, powerEnd: 252, cadence: 85),
+        Interval(duration: 180, powerStart: 154, powerEnd: 154, cadence: 85),
+        Interval(duration: 360, powerStart: 252, powerEnd: 252, cadence: 85),
+        Interval(duration: 180, powerStart: 154, powerEnd: 154, cadence: 85),
+        Interval(duration: 360, powerStart: 252, powerEnd: 252, cadence: 85),
+        Interval(duration: 180, powerStart: 154, powerEnd: 154, cadence: 85),
+      ]),
+    ];
+    Workout workout = Workout(RawWorkout(reps: reps));
+    test('Duration', () {
+      expect(workout.duration, equals(1620));
+    });
+    test('NP', () {
+      expect(workout.NP, equals(232));
+    });
+    test('TSS', () {
+      expect(workout.TSS, equals(32));
+    });
   });
 
 }
