c82449627c404cc28ed97d3d0b9285f0ff5f4f1f
vincent290587 <vincent.golle@gmail.com>
2021-08-11 16:02:00 +0200

    Added example connector

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
 # wee_planner
 
-A new Flutter project.
+Workout TSS planner uses a private workout library to schedule workouts based on a target weekly TSS and power zones.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+The software would push workouts for a given week to www.intervals.icu for easy management.
---
+++ lib/Connector.dart
@@ -0,0 +1,115 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+
+// GET /api/v1/athlete/{id}/folders List all folders and workouts
+// POST /api/v1/athlete/{id}/folders Create a folder
+// PUT /api/v1/athlete/{id}/folders/{folderId} Update folder
+// DELETE /api/v1/athlete/{id}/folders/{folderId} Delete folder
+// GET /api/v1/athlete/{id}/folders/{folderId}/shared-with Show who folder has been shared with
+// PUT /api/v1/athlete/{id}/folders/{folderId}/shared-with Update folder sharing
+// GET /api/v1/athlete/{id}/workouts List all workouts (excluding those shared by others)
+// GET /api/v1/athlete/{id}/workouts/{workoutId} Get a workout
+// POST /api/v1/athlete/{id}/workouts Create a workout
+// PUT /api/v1/athlete/{id}/workouts/{workoutId} Update a workout
+// DELETE /api/v1/athlete/{id}/workouts/{workoutId} Delete a workout
+// POST /api/v1/download-workout{ext} Download a workout in .zwo .mrc or .erg format
+
+
+Future<http.Response> postAlbum(Album album) async {
+
+  return http.post(
+    Uri.parse('https://intervals.icu/api/v1/athlete/i30899/calendars'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: album.toJson(),
+  );
+
+  // if (response.statusCode == 201) {
+  //   // If the server did return a 201 CREATED response,
+  //   // then parse the JSON.
+  //   return Album.fromJson(jsonDecode(response.body));
+  // } else {
+  //   // If the server did not return a 201 CREATED response,
+  //   // then throw an exception.
+  //   throw Exception('Failed to create album.');
+  // }
+}
+
+
+Future<Album> fetchAlbum() async {
+
+  final response = await http.post(
+    Uri.parse('https://intervals.icu/api/v1/athlete/i30899/calendars'),
+    // Send authorization headers to the backend.
+    headers: {
+      HttpHeaders.authorizationHeader: 'Basic QVBJX0tFWTo1YW5ka2w2bW1xd3FodGFuZDF4a3dpemF2',
+    },
+  );
+  final responseJson = jsonDecode(response.body);
+
+  return Album.fromJson(responseJson);
+}
+
+// {
+//   "category": "WORKOUT",
+//   "start_date_local": "2021-04-29T00:00:00",
+//   "type": "Ride",
+//   "filename": "4x8m.zwo",
+//   "file_contents": "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<workout_file>..."
+// }
+
+// reponse:
+
+// {
+//   "id": 610062,
+//   "icu_training_load": 116,
+//   ...
+//   "name": "4x8m VO2 Max",
+//   "description": "These are horribly nasty.\nCategory: Horrible\n\n- 20m 60% 90-100rpm\n\nMain set 4x\n- 8m 110%\n- 8m 50%\n\n- 10m 60%\n",
+//   ...
+//   "workout_doc": {
+//     "steps": [
+//       {
+//         "power": {"units": "%ftp", "value": 60},
+//         "cadence": {"end": 100, "start": 90, "units": "rpm"}, "duration": 1200
+//       }, ...
+//     ],
+//     "duration": 5640,
+//     "zoneTimes": [1920, 1800, 0, 0, 1920, 0, 0, 0],
+//     "hrZoneTimes": [1920, 1800, 0, 0, 1920, 0, 0, 0]
+//   },
+//   "icu_intensity": 86.04798
+// }
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'userId': userId,
+      'id': id,
+      'title': title,
+    };
+  }
+}
