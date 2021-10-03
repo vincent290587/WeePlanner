fb43a23357417a340b685c58603d9ae4736ac5b5
Vincent Golle <vincent.golle@gmail.com>
2021-10-03 12:07:38 +0200

    Added http overrides

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,16 @@ import 'WorkoutDB.dart';
 import 'WorkoutGraph.dart';
 import 'ZonesGraph.dart';
 
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = new MyHttpOverrides();
   runApp(
     ChangeNotifierProvider(
       create: (context) => WorkoutDB(),
